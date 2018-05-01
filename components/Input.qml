monero-project/monero-gui

Selection color for inputs

components/Input.qml
@@ -38,6 +38,8 @@ TextField {
     horizontalAlignment: TextInput.AlignLeft
     selectByMouse: true
     color: MoneroComponents.Style.defaultFontColor
+    selectionColor: MoneroComponents.Style.dimmedFontColor
+    selectedTextColor: MoneroComponents.Style.defaultFontColor
 
     background: Rectangle {
         color: "transparent"

components/InputMulti.qml
@@ -47,6 +47,8 @@ TextArea {
     horizontalAlignment: TextInput.AlignLeft
     selectByMouse: true
     color: MoneroComponents.Style.defaultFontColor
+    selectionColor: MoneroComponents.Style.dimmedFontColor
+    selectedTextColor: MoneroComponents.Style.defaultFontColor
 
     wrapMode: {
         if(wrapAnywhere){

msgmaxim/loki-gui

components/Input.qml
@@ -39,6 +39,8 @@ TextField {
     horizontalAlignment: TextInput.AlignLeft
     selectByMouse: true
     color: LokiComponents.Style.defaultFontColor
+    selectionColor: LokiComponents.Style.dimmedFontColor
+    selectedTextColor: LokiComponents.Style.defaultFontColor
 
     background: Rectangle {
         color: "transparent"

components/InputMulti.qml
@@ -48,6 +48,8 @@ TextArea {
     horizontalAlignment: TextInput.AlignLeft
     selectByMouse: true
     color: LokiComponents.Style.defaultFontColor
+    selectionColor: LokiComponents.Style.dimmedFontColor
+    selectedTextColor: LokiComponents.Style.defaultFontColor
 
     wrapMode: {
         if(wrapAnywhere){