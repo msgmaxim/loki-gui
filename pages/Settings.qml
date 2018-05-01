monero-project/monero-gui

Settings: do not invoke status command when using remote node

pages/Settings.qml
@@ -48,7 +48,7 @@ Rectangle {
         console.log("Settings page loaded");
 
         if(typeof daemonManager != "undefined"){
-            appWindow.daemonRunning =  daemonManager.running(persistentSettings.testnet);
+            appWindow.daemonRunning = persistentSettings.useRemoteNode ? false : daemonManager.running(persistentSettings.testnet);
         }
 
         logLevelDropdown.update()

msgmaxim/loki-gui

pages/Settings.qml
@@ -49,8 +49,8 @@ Rectangle {
     function onPageCompleted() {
         console.log("Settings page loaded");
 
-        if(typeof daemonManager != "undefined"){
-            appWindow.daemonRunning =  daemonManager.running(persistentSettings.nettype);
+        if(typeof daemonManager != "undefined") {
+            appWindow.daemonRunning = persistentSettings.useRemoteNode ? false : daemonManager.running(persistentSettings.nettype);
         }
 
         logLevelDropdown.update()